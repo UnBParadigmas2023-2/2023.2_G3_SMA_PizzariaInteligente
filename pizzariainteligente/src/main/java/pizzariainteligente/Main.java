451bc59dbd40e58a329d7d5d4228d8d63c24c525
philipeserafim <philipeserafim@gmail.com>
2023-11-29 19:57:53 -0300

    Add Main without javafx
    Co-authored-by: lucasgabriel-2 <lucasgabriel_df@hotmail.com>

---
+++ pizzariainteligente/src/main/java/pizzariainteligente/Main.java
@@ -0,0 +1,30 @@
+package pizzariainteligente;
+
+import jade.core.Runtime;
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.wrapper.AgentController;
+import jade.wrapper.ContainerController;
+
+public class Main {
+  public static void main(String[] args) {
+
+    Runtime rt = Runtime.instance();
+    Profile p = new ProfileImpl();
+    ContainerController container = rt.createMainContainer(p);
+    AgentController seller;
+
+    try {
+      seller = container.createNewAgent("recepcao", "pizzariainteligente.agents.AgenteRecepcao", null);
+      seller.start();
+      seller = container.createNewAgent("montador", "pizzariainteligente.agents.AgenteMontagem", null);
+      seller.start();
+      seller = container.createNewAgent("assador", "pizzariainteligente.agents.AgenteAssador", null);
+      seller.start();
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+}
