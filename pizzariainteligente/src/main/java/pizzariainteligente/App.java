3eb61ec346e19acd58bea95c75b3081728978edb
philipeserafim <philipeserafim@gmail.com>
2023-11-29 01:33:23 -0300

    Fix fxml

--- pizzariainteligente/src/main/java/pizzariainteligente/App.java
+++ pizzariainteligente/src/main/java/pizzariainteligente/App.java
@@ -1,38 +1,45 @@
 package pizzariainteligente;
 
+import jade.core.Runtime;
+import FIPA.stringsHolder;
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.wrapper.AgentController;
+import jade.wrapper.ContainerController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
-/**
- * JavaFX App
- */
 public class App extends Application {
+    public static void main(String[] args) {
+        launch(args);
 
-    private static Scene scene;
+        Runtime rt = Runtime.instance();
+        Profile p = new ProfileImpl();
+        ContainerController container = rt.createMainContainer(p);
+        AgentController seller;
+
+        try {
+            seller = container.createNewAgent("recepcao", "agents.AgenteRecepcao", null);
+            seller.start();
+            seller = container.createNewAgent("montador", "agents.AgenteMontagem", null);
+            seller.start();
+            seller = container.createNewAgent("assador", "agents.AgenteAssador", null);
+            seller.start();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
-
-    public static void main(String[] args) {
-        launch();
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("hellofx.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 1600, 900));
+        primaryStage.show();
     }
 
 }
